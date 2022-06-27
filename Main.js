d066376ce28efd90493662e4d1871e74e56654b8
Guido Zlatar <guido_zlatar07@hotmail.com>
2022-06-27 15:41:37 -0300

    Clase 2

--- Main.js
+++ Main.js
@@ -1,117 +1,36 @@
-import { promises } from 'fs';
-
-class Contenedor{
-    constructor (title, price, thumbnail) {
-        this.title = title
-        this.price = price
-        this.thumbnail = thumbnail
+class Usuario{
+    constructor (nombre, apellido, libros, mascotas) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.libros = libros
+        this.mascotas = mascotas
     }
 
-    async save(objeto){
-        try {
-            const contenido = await promises.readFile("./productos.txt", "utf-8")
-            const info = JSON.parse(contenido)
+    arrayMascotas = []
+    arrayLibros = []
 
-                //Asiganción del ID al objeto (Array con productos existentes)
-                if (info.length > 0) {
-                    const ultimoElemento = info[info.length -1];
-                    const id = ultimoElemento.id + 1;
-                    const objetoConId = {...objeto, id};
-                //Se agrega el objeto al array 
-                    const arrayCompleto = JSON.stringify([...info, objetoConId]);
-    
-                await promises.writeFile("./productos.txt", arrayCompleto)
-                console.log("Se ha agregado el producto")
-                console.log(`El ID asignado es ${id}`)
-                    
-                } else {
-                //Asiganción del ID al objeto (Array de productos vacío)
-                    const id = 1
-                    const objetoConId = {...objeto, id} 
-                //
-                    const arrayCompleto = JSON.stringify([...info, objetoConId])
-                    
-                await promises.writeFile("./productos.txt", arrayCompleto)
-                console.log("Se ha agregado el producto")
-                console.log(`El ID asignado es ${id}`)
-                }
-            }            
-        catch (error) {
-            console.log(error)
-            }
+    getFullName() {
+        console.log(`Nombre completo: ${this.nombre} ${this.apellido}`)
     }
 
-    async getById(numId){
-        try {
-            const contenido = await promises.readFile("./productos.txt", "utf-8");
-            const info = JSON.parse(contenido);
-
-                if (numId <= info.length && numId > 0) {
-                    console.log(info.filter(item => item.id === numId))
-                } else {
-                    console.log(null)
-                }
-          
-        } catch (error) {
-            console.log(error)
-        }
+    addMascots(mascotas){
+        this.arrayMascotas.push(mascotas)
     }
 
-    async getAll(){
-        try {
-            const contenido = await promises.readFile("./productos.txt", "utf-8");
-            const info = JSON.parse(contenido);
-            console.log(info)
-        } catch (error) { 
-            console.log(error)
-        }
+    countMascots(){
+        console.log(this.arrayMascotas.length)
     }
-
-    async deleteById(numId){
-        try {
-            const contenido = await promises.readFile("./productos.txt", "utf-8");
-            const info = JSON.parse(contenido);
-
-            if (info.map(item=>item.id === numId)) {
-                //Elimina el elemento del array
-                const idElemento = numId - 1;
-                info.splice(idElemento, 1);
-                
-                const nuevoArray = JSON.stringify(info)
-                await promises.writeFile("./productos.txt", nuevoArray)
-                console.log("El producto seleccionado ha sido eliminado")
-            } else {
-                console.log("No existe el ID ingresado")                
-            }
-        } catch (error) {
-            console.log(error)
-        }
+    addBooks(libro, autor){
+        this.arrayLibros.push({libro: libro, autor: autor})
     }
-
-    async deleteAll(){
-        try {
-            const arrayVacio = JSON.stringify([])
-            await promises.writeFile("./productos.txt", arrayVacio)
-            console.log("Todos los productos han sido eliminados")
-        } catch (error) {
-            console.log(error)
-        }
+    getBookNames(){
+        console.log(this.arrayLibros.map(item=> item.libro))
     }
 }
+const Persona1 = new Usuario("Kevin", "Suarez")
 
-//Prueba de la función Save()
-const producto1 = new Contenedor ("pelotas",200,"https://google.com.ar");
-producto1.save(producto1)
-//Se utiliza el SetTimeout asi no se superpone con el primer producto a guardar
-const producto2 = new Contenedor("transportador", 100, "https://fotos.com.ar")
-setTimeout(()=>{producto2.save(producto2)}, 1000);
-/* 
-//Prueba de la función GetById()
-producto1.getById(5);
-//Prueba de la función GetAll()
-producto1.getAll();
-//Prueba de la función DeleteById()
-producto1.deleteById(2);
-//Prueba de la función DeleteAll()
-producto1.deleteAll();
-*/
+Persona1.getFullName()
+Persona1.addMascots("Tortuga")
+Persona1.countMascots()
+Persona1.addBooks("El principito", "Saint-Exupery")
+Persona1.getBookNames()
