5698ca4fddaf5d62efb4c5ce50575c16e8a976ac
Guido Zlatar <guido_zlatar07@hotmail.com>
2022-07-21 19:57:10 -0300

    Clase 8

--- server.js
+++ server.js
@@ -1,27 +1,62 @@
 import express from 'express';
-import random from 'random'
+import Router from 'express';
 import {contenedor1 } from './main.js';
-const app = express()
+const app = express();
+const router = Router();
 
-const allProducts = await contenedor1.getAll()
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
+app.use("/", router);
 
+const allProducts = await contenedor1.getAll();
 
-app.get('/productos', (req, res)=>{
-    const resultado = JSON.stringify(allProducts)
-    
-    res.send(`<h1>${resultado}</h1>`)
+router.get('/api/productos', (req, res)=>{    
+    contenedor1.getAll().then((resp)=>res.json(resp))
+    .catch((err)=> console.log(err))
 })
 
-app.get('/productoRandom', (req, res)=>{
-    //Generacion de un número al azar
-    const numRandom = random.int(1, allProducts.length)
+router.get('/api/productos/:id', (req, res)=>{
 
-    const productId = allProducts.find(item => item.id === numRandom)
-    const resultado = JSON.stringify(productId)
+    const id = req.params.id
+
+    contenedor1.getById(id).then((resp)=> res.json(resp))
+    .catch((err)=> console.log(err))
     
-    res.send(`<h1>${resultado}</h1>`)
 })
+
+router.post('/api/productos', (req, res) => {
+    
+    const producto = {price: req.body.price, title: req.body.title, thumbnail: req.body.thumbnail}
+
+    contenedor1.save(producto).then((resp)=> res.json(resp))
+    .catch((err)=> console.log(err))
+})
+
+router.put("/api/productos/:id", (req, res) => {
+
+    const idRecibido = req.params.id
+
+/* No pude obtener los values a través de parametros usando postman postman, entonces hardcodee
+     el objeto que se va a actualizar (menos el id que eso si lo puedo obtener con params) */
+
+    const objeto = {price: 10, title: "hoja", thumbnail: "www.hoja.com.ar", id: idRecibido}
+
+    contenedor1.updateById(idRecibido, objeto).then((resp)=>res.json(resp))
+    .catch((err)=>console.log(err))
+
+})
+
+router.delete("/api/productos/:id", (req, res)=>{
+
+    const id = req.params.id
+
+    contenedor1.deleteById(id).then((resp)=> res.json(resp))
+    .catch((err)=>console.log(err))
+})
+
+
 
 const PORT = 8000
 
