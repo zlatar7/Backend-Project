d26928c3188e1994dee7d61ebae4b08074c6a1a6
Guido Zlatar <guido_zlatar07@hotmail.com>
2022-08-02 12:42:55 -0300

    Handlebars

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-import express from 'express';
-import Router from 'express';
-import {contenedor1 } from './main.js';
+const express = require('express');
 const app = express();
-const router = Router();
+const handlebars = require('express-handlebars');
+//import Router from 'express';
+//import {contenedor1 } from './main.js';
+//const router = Router();
+
+const hbs = handlebars.create({
+    extname: '.hbs',
+    defaultLayout:'index.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partliasDir: __dirname + '/views/partials/',
+})
+app.engine("hbs", hbs.engine)
+app.set("view engine", "hbs")
+app.set("views", "./views")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
 
+
+const arrayProductos = [{nombre: "mochila", precio: 5000, foto:"https://cdn3.iconfinder.com/data/icons/spring-2-1/30/Backpack-256.png"},
+                   {nombre:"regla", precio: 1000, foto:"https://cdn0.iconfinder.com/data/icons/mac-applications-icons-by-omer-cetin/128/ruler.png"},
+                   {nombre:"transportador", precio: 250, foto:"https://cdn1.iconfinder.com/data/icons/office-material-9/128/curve_ruler_protactor_ruler_set_square_stationary-128.png"},
+                   {nombre:"lapicera", precio: 120, foto:"https://cdn2.iconfinder.com/data/icons/scrap/Pen.png"}]
+
+
+app.get("/", (req, res) =>{
+    res.render("form");
+})
+app.get("/productos",(req, res)=>{
+    res.render("main",{array: arrayProductos, listExists: true})
+})
+app.post("/productos", (req, res)=>{
+    const productoRecibido = req.body
+    const productoNuevo = {nombre: productoRecibido.title , precio: productoRecibido.price, foto: productoRecibido.thumbnail};
+    const productos = [...arrayProductos, productoNuevo]
+    res.render("main", {array: productos, listExists: true})
+})
+
+/* 
 app.use("/", router);
-
 const allProducts = await contenedor1.getAll();
 
 router.get('/api/productos', (req, res)=>{    
@@ -38,9 +68,6 @@ router.put("/api/productos/:id", (req, res) => {
 
     const idRecibido = req.params.id
 
-/* No pude obtener los values a través de parametros usando postman postman, entonces hardcodee
-     el objeto que se va a actualizar (menos el id que eso si lo puedo obtener con params) */
-
     const objeto = {price: 10, title: "hoja", thumbnail: "www.hoja.com.ar", id: idRecibido}
 
     contenedor1.updateById(idRecibido, objeto).then((resp)=>res.json(resp))
@@ -56,7 +83,7 @@ router.delete("/api/productos/:id", (req, res)=>{
     .catch((err)=>console.log(err))
 })
 
-
+ */
 
 const PORT = 8000
 
